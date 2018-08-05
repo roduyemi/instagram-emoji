1107f009b6860ead7ae3d3228274ab19ad988185
rodu <remi.oduyemi@bbc.co.uk>
2018-08-05 12:40:31 +0100

    call api every hour

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const axios = require('axios');
+const cron = require('node-cron');
 
-// https://f7n9ynzq3g.execute-api.eu-west-1.amazonaws.com/prod
-const url = 'https://f7n9ynzq3g.execute-api.eu-west-1.amazonaws.com/prod';
-
-axios.get(url)
-.then(response =>  {
-  const data = response.data.body;
-  Object.keys(data).forEach(key => {
-    console.log('key : ' + key + ', value: ' + JSON.stringify(data[key]));
-  });
-})
-.catch(error => {
-  console.log(error);
+/**
+ * This cron job is scheduled to call the emojiCount API
+ * every hour, on the hour
+ */
+cron.schedule('0 * * * *', () => {
+  console.log('getting emoji count');
+  getEmojiCount();
 });
+
+getEmojiCount = () => {
+  // https://f7n9ynzq3g.execute-api.eu-west-1.amazonaws.com/prod
+  const url = 'https://f7n9ynzq3g.execute-api.eu-west-1.amazonaws.com/prod';
+
+  axios.get(url)
+  .then(response =>  {
+    const data = response.data.body;
+    Object.keys(data).forEach(key => {
+      console.log('key : ' + key + ', value: ' + JSON.stringify(data[key]));
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
