4e2facf2b775d90612566f87665b0cb975218d5b
rodu <remi.oduyemi@bbc.co.uk>
2018-08-06 19:38:52 +0100

    show emojis with force simulation

--- dataviz.js
+++ dataviz.js
@@ -11,12 +11,8 @@ var colors = [
     "#844e5e"
     ]
     
-//     var selectedValue = "tot_pop"
-//     var selectedIndex = 0
     var sizeRange = [1.5,50]
     var notcarto = true
-    
-//     // d3.csv("https://s3-eu-west-1.amazonaws.com/newslabs-geofacts/county_demo.csv", cartogram)
     
     const cartogram = (data, d3) => {
       const keys = Object.keys(data);
@@ -25,22 +21,11 @@ var colors = [
         dataArr.push(data[k])
       });
 
-      console.log('data before', data);
-      
-      // console.log('dataKeys', dataKeys);
-    //   data.forEach(d => {
-    //     dataKeys.forEach(k => {
-    //     //   data[k] = parseInt(data[k]['media_count']/5000);
-    //     });
-    //   });
       let thisKeyValues = Object.values(data);
       let values = []
       thisKeyValues.forEach(v => {
-          console.log('v', v['media_count']);
         values.push(v['media_count'])
       });
-
-      console.log('new data', dataArr);
     
       // var thisKeyValues = data.forEach((d, i) => d[i]);
       // console.log('keyValues', thisKeyValues);
@@ -59,20 +44,21 @@ var colors = [
       .on('tick', updateCartogram)
       .on('end', resetCartogram)
     
-      // doc.select('svg')
-      // d3.select('chart').append('svg')
-      d3.select('svg')
-        .append("circle")
-        .attr("class", "node")
-        .style("stroke-width", 2)
-        .style("stroke-color", 'black')
-        .selectAll('circle.node')
-        .data(dataArr)
-        .enter()
-        .append("circle")
-        .attr("class", "node")
-        .style("stroke-width", 2)
-        
+      let g = d3.select("svg")
+      .selectAll("circle.node")
+      .data(dataArr)
+      .enter()
+      .append('g')
+      // .attr("transform", "translate(0,0)")
+
+      let circle = g.append("circle")
+      .attr("class", "node")
+      .style("stroke-width", 2)
+
+      let text = g.append('text')
+      .style("text-anchor", "middle")
+      .text(d => d['name'])
+      .style('font-size', '36px')
 
       // d3.select('svg').append('text')
       //   .style('text-anchor', 'middle')
@@ -87,18 +73,17 @@ var colors = [
       function updateCartogram() {
         d3.selectAll('circle.node')
           .data(dataArr)
-          .attr('cx', d => d.x/5000)
-          .attr('cy', d => d.y/5000)
+          .attr('cx', d => d.x/3000)
+          .attr('cy', d => d.y/3000)
+
+          d3.selectAll('text')
+          .attr('x', d => d.x/3000)
+          .attr('y', d => d.y/3000)
       }
     
       function resetCartogram() {
         if (notcarto) {
-          // selectedIndex = selectedIndex === dataKeys.length ? 0 : selectedIndex + 1
-          // selectedValue = dataKeys[selectedIndex]
-          // d3.select(d3n.document.querySelector('text.title'))
-          //   .text(keys['media_count'])
-    
-          // thisKeyValues = dataCount.forEach(d => d['media_count'])
+
           extent = d3.extent(values)
     
           scale = d3.scaleLinear().domain(extent).range(sizeRange)
@@ -115,7 +100,7 @@ var colors = [
           var positionScale = d3.scaleCluster().domain(values).range([200,400,600,800,1000])
           force
           .force("x", d3.forceX(d => positionScale(d['media_count'])))
-          .force("y", d3.forceY(400))
+          .force("y", d3.forceY(200))
           .alpha(1)
           .restart()
         }
@@ -126,9 +111,10 @@ var colors = [
         d3.selectAll('circle.node')
           .data(dataArr)
           .transition()
-          .duration(500)
-          .style('fill', d => colorScale(d['media_count']))
-          .style('stroke', d => d3.hsl(colorScale(d['media_count'])).darker())
+          .duration('10s')
+          // .style('fill', d => colorScale(d['media_count']))
+          .style('fill', 'none')
+          // .style('stroke', d => d3.hsl(colorScale(d['media_count'])).darker())
           .attr('r', d => scale(d['media_count']))
         }
     }
