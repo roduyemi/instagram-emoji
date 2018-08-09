513a86596f34b8d8afc8906a83bc31d608e2929b
rodu <remi.oduyemi@bbc.co.uk>
2018-08-09 15:12:23 +0100

    cluster/split on emoji mood

--- dataviz.js
+++ dataviz.js
@@ -2,6 +2,7 @@ const cartogram = (data, d3) => {
 
   const width = window.innerWidth;;
   const height = window.innerHeight;
+  let moodForce = true;
 
   const keys = Object.keys(data);
   let dataArr = [];
@@ -42,10 +43,27 @@ const cartogram = (data, d3) => {
   .attr('xmlns:xlink', 'http://www.w3.org/1999/xlink')
   .attr('xlink:href', d => 'https://s3-eu-west-1.amazonaws.com/newslabs-geofacts/emojis/' + d['type']+ '.png')
 
+  let forceXMood = d3.forceX(d => {
+    moodForce = true
+    if (d['mood'] === 'positive') {
+      return 300
+    }
+    else {
+      return 800
+    }
+  }).strength(0.1);
+
+  let forceX = d3.forceX(d => {
+    moodForce = false
+    return width / 2
+  }).strength(0.1);
+  let forceY = d3.forceY(d => height / 2).strength(0.1);
+  let forceCollide = d3.forceCollide(d => radiusScale(d['media_count']) + 1);
+
   let simulation = d3.forceSimulation()
-    .force('x', d3.forceX(width / 2).strength(0.1))
-    .force('y', d3.forceY(height / 2).strength(0.1))
-    .force('collide', d3.forceCollide(d => radiusScale(d['media_count'])))
+    .force('x', forceXMood)
+    .force('y', forceY)
+    .force('collide', forceCollide)
 
   let div = d3.select('body').append('div')	
     .attr('class', 'tooltip')
@@ -77,10 +95,25 @@ const cartogram = (data, d3) => {
   simulation
     .nodes(dataArr)
     .on('tick', ticked)
+    // .on('end', end)
 
   function ticked() {
     circles
     .attr('cx', d =>  d.x)
     .attr('cy', d =>  d.y)
   }
+
+  d3.select('body').on('click', () => {
+    if(moodForce) {
+      simulation
+      .force('x', forceX)
+      .alphaTarget(0.25)
+      .restart()
+    } else {
+      simulation
+      .force('x', forceXMood)
+      .alphaTarget(0.5)
+      .restart()
+    }
+  })
 }
